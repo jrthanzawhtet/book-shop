1c7d48750eafe1044329f2a1cf4c71eace14a15d
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-01-12 15:16:46 +0630

    "midcommit"

---
+++ bootcamp-7-book-shop-master(1)/bootcamp-7-book-shop-master/src/main/java/com/example/bookshop/dao/CustomerDao.java
@@ -0,0 +1,10 @@
+package com.example.bookshop.dao;
+
+import com.example.bookshop.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerDao extends JpaRepository<Customer,Integer> {
+    Optional<Customer> findCustomerByCustomerName(String customerName);
+}
